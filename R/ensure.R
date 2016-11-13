f1417f61eb3788f482d50f350ac2c2750c12062f
jennybc <jenny@stat.ubc.ca>
2016-11-12 16:39:18 -0800

    edit error message

--- R/ensure.R
+++ R/ensure.R
@@ -3,13 +3,12 @@ ensure_not_empty <- function(x)
 
 ensure_not_dogfood <- function(x) {
   if (grepl("```", x[[length(x)]])) {
-    stop(paste(
-      "\nFirst three lines of putative code are:",
-      x[1], x[2], x[3],
-      "\nwhich isn't valid R code. Looks more like Markdown.",
-      "Are we going in circles? Did you just run reprex()?",
-      "In that case, the clipboard now holds the *rendered* result.",
-      sep = "\n"), call. = FALSE)
+    stop("\nFirst three lines of putative code are:",
+         x[1], x[2], x[3],
+         "which isn't valid R code. Looks more like Markdown.\n",
+         "Are we going in circles? Did you just run reprex()?\n",
+         "In that case, the clipboard now holds the *rendered* result.\n",
+         call. = FALSE)
   }
   x
 }
