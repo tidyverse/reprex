19a320d8e2cbc00da9b71c8bec4623187804f6c4
jennybc <jenny@stat.ubc.ca>
2016-11-14 13:38:10 -0800

    turn tests back on

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,6 @@ library(testthat)
 library(reprex)
 
 ## don't run tests on appveyor til pandoc sorted out
-if (!identical(tolower(Sys.getenv("APPVEYOR")), "true")) {
+#if (!identical(tolower(Sys.getenv("APPVEYOR")), "true")) {
   test_check("reprex")
-}
+#}
