b0c183e81888f18b710b0721854dee3ae19e8dd3
jennybc <jenny@stat.ubc.ca>
2016-12-23 15:32:39 -0800

    get rid of ununsed code re: building slugs, filenames, directory names

--- R/construct_names_paths.R
+++ R/construct_names_paths.R
@@ -1,17 +1,3 @@
-construct_safeslug <- function(slug = "REPREX") {
-  if(!identical(slug, "REPREX")) {
-    slug <- tolower(gsub('[^A-Za-z0-9]+', '-', slug))
-    slug <- gsub("^-|-$", '', slug)
-  }
-  slug
-}
-
-construct_filename <- function(slug = "REPREX", venue = c("gh", "so")) {
-  venue <- match.arg(venue)
-  slug <- construct_safeslug(slug)
-  paste(Sys.Date(), slug, venue, sep = "_")
-}
-
 strip_ext <- function(x, ext = "md") {
   if (is.null(x)) return(NULL)
   if (grepl(ext, tools::file_ext(x))) {
@@ -28,13 +14,4 @@ add_ext <- function(x, ext = "R", force = FALSE) {
   } else {
     x
   }
-}
-
-construct_directory <- function(rrdir = NULL) {
-  ## THIS IS JUST A STUB
-  ## I HAVE BIG PLANS
-  if(is.null(rrdir)) {
-    rrdir <- getwd()
-  }
-  normalizePath(rrdir)
 }
