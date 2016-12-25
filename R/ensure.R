839b433df1efc1893358a3aec4bb02b18476b015
jennybc <jenny@stat.ubc.ca>
2016-12-24 18:08:04 -0800

    Tweak error message

--- R/ensure.R
+++ R/ensure.R
@@ -2,10 +2,10 @@ ensure_not_empty <- function(x)
   if (length(x) < 1) read_from_template("BETTER_THAN_NOTHING") else x
 
 ensure_not_dogfood <- function(x) {
-  if (grepl("```", x[[length(x)]])) {
-    stop("\nFirst three lines of putative code are:",
-         x[1], x[2], x[3],
-         "which isn't valid R code. Looks more like Markdown.\n",
+  if (grepl("```", utils::tail(x, 1))) {
+    stop("First three lines of putative code are:\n",
+         paste(x[1:3], collapse = "\n"),
+         "\nwhich isn't valid R code. Looks more like Markdown.\n",
          "Are we going in circles? Did you just run reprex()?\n",
          "In that case, the clipboard now holds the *rendered* result.\n",
          call. = FALSE)
