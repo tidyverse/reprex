fc89f50984658fff6cdb2d3cf3b13fe2d75d0c9f
jennybc <jenny@stat.ubc.ca>
2016-12-26 23:25:25 -0800

    something that sort of works

--- R/reprex-addin.R
+++ R/reprex-addin.R
@@ -1,54 +1,67 @@
 reprex_addin <- function() {
 
-  # Our ui will be a simple gadget page, which
-  # simply displays the time in a 'UI' output.
+  reprex_link <- tags$a(href = "https://github.com/jennybc/reprex#readme", "reprex")
+
   ui <- miniUI::miniPage(
-    miniUI::gadgetTitleBar("Clock"),
+    miniUI::gadgetTitleBar("Prepare a reprex"),
     miniUI::miniContentPanel(
-      shiny::uiOutput("time")
+      h4("Use ", reprex_link, " to render snippets of code."),
+      hr(),
+      selectInput("source",
+                  "Where is reprex source?",
+                  c("clipboard", "current selection",
+                    "current file", "another file")
+      ),
+      uiOutput("document", container = rCodeContainer),
+      miniUI::miniButtonBlock(
+        actionButton("reprex", "Render")
+      )
     )
   )
 
   server <- function(input, output, session) {
 
-    # Set some CSS styles for our clock.
-    clockStyles <- paste(
-      "border: 1px solid #DADADA",
-      "background-color: #EFEFEF",
-      "border-radius: 5px",
-      "font-size: 6em",
-      "margin-top: 60px",
-      "text-align: center",
-      sep = "; "
-    )
+    context <- rstudioapi::getActiveDocumentContext()
 
-    # We'll use a 'reactiveTimer()' to force Shiny
-    # to update and show the clock every second.
-    invalidatePeriodically <- shiny::reactiveTimer(intervalMs = 1000)
-    shiny::observe({
+    reactiveDocument <- reactive({
 
-      # Call our reactive timer in an 'observe' function
-      # to ensure it's repeatedly fired.
-      invalidatePeriodically()
+      reprex_source <- input$source
 
-      # Get the time, and render it as a large paragraph element.
-      time <- Sys.time()
-      output$time <- shiny::renderUI({
-        p(style = clockStyles, time)
-      })
+      if (identical(reprex_source, "clipboard")) {
+        res <- reprex()
+      } else {
+        res <- reprex(src = "mean(rnorm(10))")
+      }
+      res
     })
 
-    # Listen for 'done' events. When we're finished, we'll
-    # insert the current time, and then stop the gadget.
+    output$document <- renderCode({
+      document <- reactiveDocument()
+      document
+    })
+
     shiny::observeEvent(input$done, {
-      timeText <- paste0("\"", as.character(Sys.time()), "\"")
-      rstudioapi::insertText(timeText)
-      shiny::stopApp()
+      contents <- paste(reactiveDocument(), collapse = "\n")
+      rstudioapi::setDocumentContents(contents, id = context$id)
+      invisible(shiny::stopApp())
     })
 
   }
 
-  viewer <- shiny::dialogViewer("reprex")
+  viewer <- shiny::dialogViewer("Reformat Code", width = 1000, height = 800)
   shiny::runGadget(ui, server, viewer = viewer)
 
+}
+
+rCodeContainer <- function(...) {
+  code <- shiny::HTML(as.character(tags$code(class = "language-r", ...)))
+  shiny::div(shiny::pre(code))
+}
+
+renderCode <- function(expr, env = parent.frame(), quoted = FALSE) {
+  func <- NULL
+  shiny::installExprFunction(expr, "func", env, quoted)
+  shiny::markRenderFunction(textOutput, function() {
+    paste(func(), collapse = "\n")
+  })
 }
