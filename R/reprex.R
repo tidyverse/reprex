40ba2c3cb81aefb2082277ea0ad40f3cb16055eb
Hiroaki Yutani <yutani.ini@gmail.com>
2017-05-26 07:58:46 +0900

    Fix UTF-8-related problem on Windows (#76)
    * set UTF-8 on output files
    
    * attempt to fix tests

--- R/reprex.R
+++ R/reprex.R
@@ -246,7 +246,7 @@ reprex <- function(
     pathstem <- path_stem(r_file, md_file)
     message("Writing reprex markdown:\n  * ", sub(pathstem, "", md_file))
   }
-  output_lines <- readLines(md_file)
+  output_lines <- readLines(md_file, encoding = "UTF-8")
 
   if (identical(venue, "r")) {
     lns <- output_lines
@@ -281,12 +281,12 @@ reprex <- function(
     ## `clean = FALSE` does too much (deletes foo_reprex_files, which might
     ## hold local figs)
     if (is.null(outfile)) {
-      html_file <- rmarkdown::render(md_file, quiet = TRUE)
+      html_file <- rmarkdown::render(md_file, quiet = TRUE, encoding = "UTF-8")
     } else {
       html_file <- strip_ext(basename(md_file))
       html_file <- tempfile(pattern = paste0(html_file, "_"), fileext = ".html")
       html_file <-
-        rmarkdown::render(md_file, output_file = html_file, quiet = TRUE)
+        rmarkdown::render(md_file, output_file = html_file, quiet = TRUE, encoding = "UTF-8")
     }
     viewer <- getOption("viewer") %||% utils::browseURL
     viewer(html_file)
