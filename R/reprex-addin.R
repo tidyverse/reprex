e553283f4b1d2456f950254f0d6c30eceea7add1
Hadley Wickham <h.wickham@gmail.com>
2017-05-25 15:51:38 -0700

    Make reprex add in a little quieter (#83)
    * Reduce reprex addin output
    
    * Hide shiny port message
    * Don't use modal dialog
    
    * Run reprex after gadget closes

--- R/reprex-addin.R
+++ R/reprex-addin.R
@@ -74,34 +74,21 @@ reprex_addin <- function() { # nocov start
   )
 
   server <- function(input, output, session) {
-
     shiny::observeEvent(input$done, {
-      reprex_output <- reprex_guess(
+      shiny::stopApp(list(
         input$source,
         input$venue,
         input$source_file,
         as.logical(input$si),
         as.logical(input$show)
-      )
-
-      shiny::showModal(
-        shiny::modalDialog(
-          "Rendered reprex is on the clipboard.",
-          footer = shiny::actionButton("ok", "OK")
-        )
-      )
-      #reprex_output <- paste(reprex_output, collapse = "\n")
-      #rstudioapi::insertText(Inf, reprex_output, id = context$id)
+      ))
     })
-
-    shiny::observeEvent(input$ok, {
-      invisible(shiny::stopApp())
-    })
-
   }
 
-  shiny::runGadget(ui, server, viewer = shiny::dialogViewer("Render reprex"))
+  app <- shiny::shinyApp(ui, server, options = list(quiet = TRUE))
+  result <- shiny::runGadget(app, viewer = shiny::dialogViewer("Render reprex"))
 
+  do.call(reprex_guess, result)
 }
 
 reprex_guess <- function(source, venue = "gh", source_file = NULL,
