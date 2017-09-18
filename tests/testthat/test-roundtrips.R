e4798182812b8395567516d463650d1d10331b75
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2017-09-17 19:06:15 -0700

    Handle continuation lines; fixes #95 (#102)

--- tests/testthat/test-roundtrips.R
+++ tests/testthat/test-roundtrips.R
@@ -4,14 +4,19 @@ input <- c(
   "## a comment",
   "x <- 1:4",
   "#' hi",
-  "y <- 2:5",
+  "y <- c(2,",
+  "       3, 4,",
+  "       5)",
   "x + y"
 )
 
 test_that("round trip: reprex(..., venue = 'R') --> reprex_clean()", {
-  output <-
-    reprex(input = input, show = FALSE, venue = "R",
-           opts_chunk = list(comment = "#!"))
+  output <- reprex(
+    input = input,
+    show = FALSE,
+    venue = "R",
+    opts_chunk = list(comment = "#!")
+  )
   expect_message(res <- reprex_clean(output, "^#!"))
   expect_identical(input, res[nzchar(res)])
 })
@@ -19,7 +24,8 @@ test_that("round trip: reprex(..., venue = 'R') --> reprex_clean()", {
 test_that("round trip: reprex(..., venue = 'gh') --> reprex_invert()", {
   output <- reprex(input = input, show = FALSE)
   expect_message(res <- reprex_invert(output))
-  expect_identical(input, res)
+  ## TO DO: return here after adopting styler to see if can remove trim_ws()
+  expect_identical(trim_ws(input), trim_ws(res))
 })
 
 test_that("round trip: reprex(..., venue = 'so') --> reprex_invert()", {
@@ -31,15 +37,19 @@ test_that("round trip: reprex(..., venue = 'so') --> reprex_invert()", {
 test_that("code can be rescued from R Console copy/paste", {
   input <- c(
     "> ## a regular comment, which is retained",
-    "> (x <- 1:4)",
-    "[1] 1 2 3 4",
-    "> median(x)",
-    "[1] 2.5"
+    "> (y <- c(2,",
+    "+         3, 4,",
+    "+         5))",
+    "[1] 2 3 4 5",
+    "> median(y)",
+    "[1] 3.5"
   )
   output <- c(
     "## a regular comment, which is retained",
-    "(x <- 1:4)",
-    "median(x)"
+    "(y <- c(2,",
+    "        3, 4,",
+    "        5))",
+    "median(y)"
   )
   expect_message(res <- reprex_rescue(input))
   expect_identical(res, output)
@@ -59,6 +69,27 @@ test_that("prompt argument works", {
     "median(x)"
   )
   expect_message(res <- reprex_rescue(input, prompt = ":-) "))
+  expect_identical(res, output)
+})
+
+test_that("continue argument works", {
+  input <- c(
+    "> ## a regular comment, which is retained",
+    "> (y <- c(2,",
+    "yes, and?         3, 4,",
+    "yes, and?         5))",
+    "[1] 2 3 4 5",
+    "> median(y)",
+    "[1] 3.5"
+  )
+  output <- c(
+    "## a regular comment, which is retained",
+    "(y <- c(2,",
+    "        3, 4,",
+    "        5))",
+    "median(y)"
+  )
+  expect_message(res <- reprex_rescue(input, continue = "yes, and? "))
   expect_identical(res, output)
 })
 
