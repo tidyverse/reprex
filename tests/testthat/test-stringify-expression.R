e3f434461e6af2d5cc421a73779fb94c9db623f0
Jim Hester <james.f.hester@gmail.com>
2017-09-25 10:25:52 -0400

    Simplify stringify_expression()
    This should now retain original formatting, so we should not need to
    reformat with formatR / styler

--- tests/testthat/test-stringify-expression.R
+++ tests/testthat/test-stringify-expression.R
@@ -1,11 +1,11 @@
 context("expression stringification")
 
 test_that("one statement, naked", {
-  expect_identical(stringify_expression(1:5), "1:5")
+  expect_identical(stringify_expression(quote(1:5)), "1:5")
 })
 
 test_that("one statement, brackets, one line", {
-  expect_identical(stringify_expression({1:5}), "1:5")
+  expect_identical(stringify_expression(quote({1:5})), "1:5")
 })
 
 test_that("one statement, quoted, one line", {
@@ -13,9 +13,9 @@ test_that("one statement, quoted, one line", {
 })
 
 test_that("one statement, brackets, multiple lines, take 1", {
-  expect_identical(stringify_expression({
+  expect_identical(stringify_expression(quote({
     1:5
-  }), "1:5")
+  })), "    1:5")
 })
 
 test_that("one statement, brackets, multiple lines, take 2", {
@@ -24,22 +24,14 @@ test_that("one statement, brackets, multiple lines, take 2", {
 })
 
 test_that("one statement, brackets, multiple lines, take 3", {
-  expect_identical(stringify_expression({
-    1:5}), "1:5")
+  expect_identical(
+    stringify_expression(quote({
+    1:5})), "    1:5")
 })
 
-# mystery to solve
-#
-# trying to test an expression like this:
-# reprex({1:3;4:6})
-# which appears to work interactively
-# but every way I think to test it fails :(
-# test_that("multiple statements, brackets", {
-#   expect_identical(stringify_expression(quote({1:3;4:6})), "1:3;4:6")
-# })
-# print(stringify_expression(quote({1:3;4:6})))
-# I have to use quote, but then that causes extra stuff to be absorbed into expr
-# update: I think the semicolon is a big part of the story/problem
+test_that("multiple statements, brackets", {
+   expect_identical(stringify_expression(quote({1:3;4:6})), "1:3;4:6")
+})
 
 test_that("leading comment", {
   ret <- stringify_expression(quote({
