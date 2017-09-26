d0d22d97c020f90b8b8dcce5f103d0132c8e0967
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-09-26 16:57:18 -0700

    Tests re: stdout

---
+++ tests/testthat/test-stdout-stderr.R
@@ -0,0 +1,13 @@
+context("stdout-stderr")
+
+test_that("stdout is captured", {
+  out <- reprex(system2("echo", args = 'blah'), std_out_err = TRUE, show = FALSE)
+  expect_match(out, "standard output and standard error", all = FALSE)
+  expect_match(out, "#> blah", all = FALSE)
+})
+
+test_that("stdout placeholder appears if nothing is captured", {
+  out <- reprex(1:4, std_out_err = TRUE, show = FALSE)
+  expect_match(out, "standard output and standard error", all = FALSE)
+  expect_match(out, "#> nothing written to stdout or stderr", all = FALSE)
+})
