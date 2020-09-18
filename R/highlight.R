e3533fae3d1473e8ae8c96f79868e9ce7b426d99
assignUser <github@krushak.me>
2020-09-18 19:35:24 +0200

    typo

--- R/highlight.R
+++ R/highlight.R
@@ -1,6 +1,6 @@
 reprex_highlight <- function(rout_file, reprex_file, arg_string = NULL) {
   arg_string <- arg_string %||% highlight_args()
-  out <- ifelse(Platform$OS.type == "windows", " -o ", " > ")
+  out <- ifelse(.Platform$OS.type == "windows", " -o ", " > ")
   cmd <- paste0(
     "highlight ", rout_file,
     " --out-format=rtf --no-trailing-nl --encoding=UTF-8",
