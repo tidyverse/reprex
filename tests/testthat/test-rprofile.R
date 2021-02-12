b23ff181c28bccf99344dcb0b308ac27da3ab476
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-02-12 14:11:28 -0800

    Message the user about local .Rprofile

--- tests/testthat/test-rprofile.R
+++ tests/testthat/test-rprofile.R
@@ -1,17 +1,31 @@
-test_that(".Rprofile local to reprex target directory is consulted", {
-  local_temp_wd("reprextests-aaa")
+test_that(".Rprofile local to reprex target directory is consulted & messaged", {
+  local_reprex_loud()
+
+  local_temp_wd("reprextests-aaa-")
   cat("x <- 'aaa'\n", file = ".Rprofile")
   cat("x\n", file = "foo.R")
   aaa_foo <- path_abs("foo.R")
 
-  local_temp_wd("reprextests-bbb")
+  local_temp_wd("reprextests-bbb-")
   cat("x <- 'bbb'\n", file = ".Rprofile")
 
-  out <- reprex(x, outfile = NA, advertise = FALSE)
+  msg <- capture_messages(
+    out <- reprex(x, outfile = NA, advertise = FALSE)
+  )
   expect_match(out, "bbb", all = FALSE)
+  expect_messages_to_include(
+    msg,
+    c("Local `[.]Rprofile` detected", "bbb")
+  )
 
-  out <- reprex(input = aaa_foo, outfile = NA, advertise = FALSE)
+  msg <- capture_messages(
+    out <- reprex(input = aaa_foo, outfile = NA, advertise = FALSE)
+  )
   expect_match(out, "aaa", all = FALSE)
+  expect_messages_to_include(
+    msg,
+    c("Local `[.]Rprofile` detected", "aaa")
+  )
 })
 
 test_that("local .Rprofile reporting responds to venue", {
@@ -19,9 +33,10 @@ test_that("local .Rprofile reporting responds to venue", {
   expect_snapshot(rprofile_alert("r"))
 })
 
-test_that("local .Rprofile is reported", {
-  local_temp_wd()
-  cat("x <- 'aaa'\n", file = ".Rprofile")
-  out <- reprex(x, outfile = NA, advertise = FALSE)
-  expect_match(out, "Local `.Rprofile` detected", fixed = TRUE, all = FALSE)
+test_that("local .Rprofile not reported when it's not there", {
+  local_reprex_loud()
+  msg <- capture_messages(
+    reprex(1 + 1, advertise = FALSE)
+  )
+  expect_false(any(grepl(".Rprofile", msg, fixed = TRUE)))
 })
