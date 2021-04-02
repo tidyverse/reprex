37fd0a82fd460d91848355c46b55369bcd89012a
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-04-02 09:20:01 -0700

    Put code in braces to satisfy with_mock()

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,17 +1,23 @@
 test_that("locate_input() works", {
   with_mock(
     reprex_clipboard = function() TRUE,
-    expect_identical("clipboard", locate_input(NULL))
+    {
+      expect_identical("clipboard", locate_input(NULL))
+    }
   )
   with_mock(
     reprex_clipboard = function() FALSE,
     in_rstudio = function() TRUE,
-    expect_identical("selection", locate_input(NULL))
+    {
+      expect_identical("selection", locate_input(NULL))
+    }
   )
   with_mock(
     reprex_clipboard = function() FALSE,
     in_rstudio = function() FALSE,
-    expect_null(locate_input(NULL))
+    {
+      expect_null(locate_input(NULL))
+    }
   )
   expect_identical("path", locate_input(path_temp()))
   expect_identical("input", locate_input(c("a", "b")))
