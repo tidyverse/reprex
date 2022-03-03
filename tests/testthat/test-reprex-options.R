26e3e913cc92940bd51934861081a2f4854ba3f6
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-03-02 16:32:57 -0800

    Fall more in line with use_r() / use_test() conventions
    I don't embrace fully because the fact that this package basically has 1 main function would mean putting too many tests in a single test file.
    
    I think it's OK to break out certain topics into individual test files.

--- tests/testthat/test-reprex-options.R
+++ tests/testthat/test-reprex-options.R
@@ -6,3 +6,48 @@ test_that("reprex.current_venue is set", {
   ret <- reprex(input = paste0(input, "\n"), venue = "html")
   expect_match(ret, "html", all = FALSE)
 })
+
+test_that("`session_info` can be set via option", {
+  skip_on_cran()
+  withr::with_options(
+    list(reprex.session_info = TRUE),
+    out <- reprex(1, render = FALSE)
+  )
+  expect_match(out, "session_*[iI]nfo", all = FALSE)
+})
+
+test_that("`advertise` can be set via option", {
+  skip_on_cran()
+  withr::with_options(
+    list(reprex.advertise = FALSE),
+    out <- reprex(1, render = FALSE)
+  )
+  expect_false(any(grepl("#+ reprex-ad", out, fixed = TRUE)))
+})
+
+test_that("`comment` can be set via option", {
+  skip_on_cran()
+  withr::with_options(
+    list(reprex.comment = "#? "),
+    out <- reprex(rnorm(1))
+  )
+  expect_match(out, "^#\\?", all = FALSE)
+})
+
+test_that("`tidyverse_quiet` can be set via option", {
+  skip_on_cran()
+  withr::with_options(
+    list(reprex.tidyverse_quiet = FALSE),
+    out <- reprex(mean(1:3), render = FALSE)
+  )
+  expect_match(out, "tidyverse_quiet: FALSE", fixed = TRUE, all = FALSE)
+})
+
+test_that("`std_out_err` can be set via option", {
+  skip_on_cran()
+  withr::with_options(
+    list(reprex.std_out_err = TRUE),
+    out <- reprex(1, render = FALSE)
+  )
+  expect_match(out, "std_out_err", all = FALSE)
+})
--- tests/testthat/test-utils-io.R
+++ tests/testthat/test-utils-io.R
@@ -41,3 +41,49 @@ test_that("we don't add a suffix more than once", {
   x <- "blah_r.R"
   expect_equal(x, add_suffix(x, suffix = "r"))
 })
+
+test_that("make_filebase() works with no input", {
+  x <- make_filebase(infile = NULL, wd = NULL)
+
+  filebase_base <- path_file(x)
+  # adjective-animal
+  expect_match(filebase_base, "^[a-z]+[-][a-z]+$")
+
+  filebase_parent <- path_file(path_dir(x))
+  # reprex-[hexademical from tempfile()]-adjective-animal
+  expect_match(filebase_parent, "^reprex-[[:xdigit:]]+[-][a-z]+[-][a-z]+$")
+
+  temp <- path_real(path_temp())
+  expect_identical(path_common(c(x, temp)), temp)
+})
+
+test_that("make_filebase(wd = '.') works", {
+  x <- make_filebase(infile = NULL, wd = ".")
+  expect_equal(path_dir(x), ".")
+  # adjective-animal
+  expect_match(x, "^[a-z]+[-][a-z]+$")
+})
+
+test_that("make_filebase(wd = 'blah') works", {
+  wd <- path_temp("xyz")
+  x <- make_filebase(infile = NULL, wd = wd)
+  expect_equal(path_file(path_dir(x)), "xyz")
+  # adjective-animal
+  expect_match(path_file(x), "^[a-z]+[-][a-z]+$")
+})
+
+test_that("make_filebase(infile = 'blah') works", {
+  # relative path
+  expect_equal(make_filebase(infile = "foo.R"), "foo")
+  expect_equal(make_filebase(infile = "blah/foo.R"), "blah/foo")
+
+  # `wd` should be ignored
+  expect_equal(make_filebase(infile = "foo.R", wd = "wut"), "foo")
+  expect_equal(make_filebase(infile = "blah/foo.R", wd = "wut"), "blah/foo")
+
+  # absolute path
+  infile <- path_temp("abcde.R")
+  x <- make_filebase(infile = infile)
+  expect_equal(path_file(x), "abcde")
+  expect_equal(path_dir(x), path_dir(infile))
+})
