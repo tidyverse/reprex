c07f90570b30516e3c6dc7138f66ac3060fa37fc
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-09-03 21:25:31 -0700

    Grab the github CSS

---
+++ data-raw/github-css.R
@@ -0,0 +1,18 @@
+# use the CSS found here:
+# https://github.com/sindresorhus/github-markdown-css
+# which aims to be:
+# "The minimal amount of CSS to replicate the GitHub Markdown style"
+
+library(usethis)
+
+use_github_file(
+  repo_spec = "sindresorhus/github-markdown-css",
+  path = "github-markdown-dark.css",
+  save_as = "inst/rmarkdown/templates/reprex_document/resources/github-dark.css"
+)
+
+use_github_file(
+  repo_spec = "sindresorhus/github-markdown-css",
+  path = "github-markdown-light.css",
+  save_as = "inst/rmarkdown/templates/reprex_document/resources/github-light.css"
+)
