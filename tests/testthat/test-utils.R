fc307bfc935ce9dc5784dcb7fced31d77fb33757
Nic Crane <thisisnic@gmail.com>
2026-05-27 09:12:19 +0100

    Add mocked test update

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -11,6 +11,7 @@ test_that("locate_input() works", {
   with_mocked_bindings(
     reprex_clipboard = function() FALSE,
     in_rstudio = function() FALSE,
+    in_positron = function() FALSE,
     expect_snapshot(locate_input(NULL), error = TRUE)
   )
   expect_identical("path", locate_input(path_temp()))
